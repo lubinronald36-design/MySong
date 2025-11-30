a635b40b013ea507dac2b2e0df2cc191044b9515
lubinronald36-design <lubinronald36@gmail.com>
2025-11-29 23:06:12 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,47 @@
+
+
+// Update your JavaScript code to handle button clicks
 const s1 = new Audio('s1.wav');
 const s2 = new Audio('s2.wav');
 const s3 = new Audio('s3.wav');
-
 const animation = document.querySelector('.animation');
+const buttons = document.querySelectorAll('.key-btn');
 
 document.addEventListener('keydown', (e) => {
-    switch (e.key) {
-        case 'c':
-            s1.play();
-            animation.classList.add('c-animation');
-            setTimeout(() => {
-                animation.classList.remove('c-animation');
-            }, 500);
-            break;
-        case 'g':
-            s2.play();
-            animation.classList.add('g-animation');
-            setTimeout(() => {
-                animation.classList.remove('g-animation');
-            }, 500);
-            break;
-        case 'd':
-            s3.play();
-            animation.classList.add('d-animation');
-            setTimeout(() => {
-                animation.classList.remove('d-animation');
-            }, 500);
-            break;
-        default:
-            break;
-    }
+  playSound(e.key);
 });
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const key = button.dataset.key;
+    playSound(key);
+  });
+});
+
+function playSound(key) {
+  switch (key) {
+    case 'c':
+      s1.play();
+      animation.classList.add('c-animation');
+      setTimeout(() => {
+        animation.classList.remove('c-animation');
+      }, 500);
+      break;
+    case 'g':
+      s2.play();
+      animation.classList.add('g-animation');
+      setTimeout(() => {
+        animation.classList.remove('g-animation');
+      }, 500);
+      break;
+    case 'd':
+      s3.play();
+      animation.classList.add('d-animation');
+      setTimeout(() => {
+        animation.classList.remove('d-animation');
+      }, 500);
+      break;
+    default:
+      break;
+  }
+}
